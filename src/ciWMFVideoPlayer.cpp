75191767a31f9d4fdbe8cebbbfd5a28a3b6777c7
ritesh <riteshlala.ed@gmail.com>
2016-09-13 14:07:51 -0400

    fix video orientation

--- src/ciWMFVideoPlayer.cpp
+++ src/ciWMFVideoPlayer.cpp
@@ -130,8 +130,8 @@ bool ciWMFVideoPlayer::loadMovie( const fs::path &filePath, const string &audioD
 		gl::Texture::Format format;
 		format.setInternalFormat(GL_RGBA);
 		format.setTargetRect();
+		format.loadTopDown(true);
 		_tex = gl::Texture::create(_width,_height, format);
-		//_tex.allocate(_width,_height,GL_RGBA,true);
 		_player->m_pEVRPresenter->createSharedTexture(_width, _height, _tex->getId());
 		_sharedTextureCreated = true;
 	}
@@ -147,8 +147,8 @@ bool ciWMFVideoPlayer::loadMovie( const fs::path &filePath, const string &audioD
 			gl::Texture::Format format;
 			format.setInternalFormat(GL_RGBA);
 			format.setTargetRect();
+			format.loadTopDown(true);
 			_tex = gl::Texture::create(_width,_height, format);
-			//_tex.allocate(_width,_height,GL_RGBA,true);
 			_player->m_pEVRPresenter->createSharedTexture(_width, _height, _tex->getId());
 		}
 	}
