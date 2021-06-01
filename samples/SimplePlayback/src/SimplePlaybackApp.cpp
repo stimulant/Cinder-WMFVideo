d5bcc1556f792d22eeac26b53c4f9e20468fb957
nathan lachenmyer <nathan@sitara.systems>
2021-05-31 17:16:18 -0700

    working .vcxproj that produces a static lib

--- samples/SimplePlayback/src/SimplePlaybackApp.cpp
+++ samples/SimplePlayback/src/SimplePlaybackApp.cpp
@@ -1,38 +1,36 @@
 #include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "cinder/params/Params.h"
-
-#include "ciWMFVideoPlayer.h"
-
-class SimplePlaybackApp : public ci::app::App
-{
-public:
-	SimplePlaybackApp();
-	~SimplePlaybackApp();
-
-	void draw();
-	void mouseDown( ci::app::MouseEvent event ) override;
-	void mouseDrag( ci::app::MouseEvent event ) override;
-	void setup();
-	void update();
-
-private:
-	bool						mVideoSetup;
-	ciWMFVideoPlayer			mVideo1;
-
-	ci::params::InterfaceGlRef	mParams;
-	float						mFps;
-};
-
 #include "cinder/app/RendererGl.h"
 #include "cinder/Log.h"
+#include "ciWMFVideoPlayer.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class SimplePlaybackApp : public App {
+	public:
+		SimplePlaybackApp();
+		~SimplePlaybackApp();
+		void setup();
+		void update();
+		void draw();
+		void mouseDown(ci::app::MouseEvent event) override;
+	private:
+		bool mVideoSetup;
+		ciWMFVideoPlayer mVideo1;
+		ci::params::InterfaceGlRef mParams;
+		float mFps;
+};
+
 SimplePlaybackApp::SimplePlaybackApp() :
-	mFps( 0.0f )
+	mFps(0.0f)
 {
 }
 
@@ -40,46 +38,37 @@ SimplePlaybackApp::~SimplePlaybackApp()
 {
 }
 
-void SimplePlaybackApp::draw()
-{
-	gl::clear( Color( 0, 0, 0 ) );
-	mVideo1.draw( 0, 0 );
-
-	mParams->draw();
-}
-
-void SimplePlaybackApp::setup()
-{
-	std::string videoPath = getAssetPath( "1.wmv" ).string();
-	mVideo1.loadMovie( videoPath, "Headphones (High Definition Audio Device)" );
+void SimplePlaybackApp::setup() {
+	std::string videoPath = getAssetPath("1.wmv").string();
+	mVideo1.loadMovie(videoPath, "Headphones (High Definition Audio Device)");
 	mVideo1.play();
 	mVideo1.setLoop(true);
-	mVideo1.getPresentationEndedSignal().connect( []() {
+	mVideo1.getPresentationEndedSignal().connect([]() {
 		ci::app::console() << "Video finished playing!" << std::endl;
-	} );
+		});
 
-	mParams = params::InterfaceGl::create( "Params", ivec2( 210, 210 ) );
-	mParams->addParam<float>( "FPS", &mFps, true );
+	mParams = params::InterfaceGl::create("Params", ivec2(210, 210));
+	mParams->addParam<float>("FPS", &mFps, true);
 }
 
-void SimplePlaybackApp::mouseDown( MouseEvent event )
-{
-	CI_LOG_I( "Video position: " << mVideo1.getPosition() );
+void SimplePlaybackApp::mouseDown( MouseEvent event ) {
+	CI_LOG_I("Video position: " << mVideo1.getPosition());
 }
 
-void SimplePlaybackApp::mouseDrag( MouseEvent event )
-{
-}
-
-void SimplePlaybackApp::update()
-{
+void SimplePlaybackApp::update() {
 	mFps = getAverageFps();
 	mVideo1.update();
 }
 
-CINDER_APP( SimplePlaybackApp, RendererGl, []( App::Settings * settings )
-{
-	settings->setFrameRate( 60.0f );
-	settings->prepareWindow(Window::Format().size(1280, 720).title("Simple Playback"));
-	settings->setConsoleWindowEnabled(true);
-} )
+void SimplePlaybackApp::draw() {
+	gl::clear(Color(0, 0, 0));
+	mVideo1.draw(0, 0);
+
+	mParams->draw();
+}
+
+CINDER_APP(SimplePlaybackApp, RendererGl, [](App::Settings* settings) {
+		settings->setFrameRate(60.0f);
+		settings->prepareWindow(Window::Format().size(1280, 720).title("Simple Playback"));
+		settings->setConsoleWindowEnabled(true);
+})
